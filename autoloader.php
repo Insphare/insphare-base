8f8325d6776ca76bbcd27f9a7e39d9eddcc5dec0
Manuel <insphare@gmx.de>
2015-04-23 01:36:06 +0200

    initial commit

--- autoloader.php
+++ autoloader.php
@@ -6,16 +6,10 @@ use Insphare\Base\ObjectContainer;
 $ds = DIRECTORY_SEPARATOR;
 include_once implode($ds, array(__DIR__, 'lib', 'insphare', 'Base', 'Autoloader.php'));
 
-$arrIncludePath = array(
-	'Insphare' => __DIR__ . $ds . 'lib' . $ds . 'insphare',
-);
-
-foreach ($arrIncludePath as $namespace => $includePath) {
-	$autoloader = new \Insphare\Base\Autoloader();
-	$autoloader->addIncludePath($includePath);
-	$autoloader->setNameSpace($namespace);
-	$autoloader->register();
-}
+$autoloader = new \Insphare\Base\Autoloader();
+$autoloader->addIncludePath(__DIR__ . $ds . 'lib' . $ds . 'insphare');
+$autoloader->setNameSpace('Insphare');
+$autoloader->register();
 
 $container = new ObjectContainer();
 $container->setSetup(new Setup());
